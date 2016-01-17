d5d58c3e3b7cfc53dbe35164ff04a3e647ba3b94
Sonia Keys <soniakeys@gmail.com>
2016-01-16 19:48:38 -0500

    a start maybe

---
+++ dot/dot.go
@@ -0,0 +1,412 @@
+package dot
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"strconv"
+
+	"github.com/soniakeys/graph"
+)
+
+type AttrVal struct {
+	Attr string
+	Val  string
+}
+
+type Config struct {
+	Indent       string
+	Directed     bool
+	UndirectArcs bool
+	NodeLabel    func(graph.NI) string
+	EdgeLabel    func(int) string
+	GraphAttr    []AttrVal
+	RankLeaves   bool
+}
+
+var Defaults = Config{
+	Indent:    "  ",
+	Directed:  true,
+	NodeLabel: func(n graph.NI) string { return strconv.Itoa(int(n)) },
+	EdgeLabel: func(l int) string { return strconv.Itoa(l) },
+}
+
+func Indent(i string) func(*Config) {
+	return func(c *Config) { c.Indent = i }
+}
+
+func Directed(d bool) func(*Config) {
+	return func(c *Config) { c.Directed = d }
+}
+
+func NodeLabel(f func(graph.NI) string) func(*Config) {
+	return func(c *Config) { c.NodeLabel = f }
+}
+
+func EdgeLabel(f func(int) string) func(*Config) {
+	return func(c *Config) { c.EdgeLabel = f }
+}
+
+func GraphAttr(attr, val string) func(*Config) {
+	return func(c *Config) {
+		for i := len(c.GraphAttr) - 1; i >= 0; i-- {
+			if c.GraphAttr[i].Attr == attr {
+				c.GraphAttr[i].Val = val
+				return
+			}
+		}
+		c.GraphAttr = append(c.GraphAttr, AttrVal{attr, val})
+	}
+}
+
+func RankLeaves(r bool) func(*Config) {
+	return func(c *Config) { c.RankLeaves = r }
+}
+
+func UndirectArcs(u bool) func(*Config) {
+	return func(c *Config) { c.UndirectArcs = u }
+}
+
+func StringAdjacencyList(g graph.AdjacencyList, options ...func(*Config)) (string, error) {
+	var b bytes.Buffer
+	if err := WriteAdjacencyList(g, &b, options...); err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
+
+func WriteAdjacencyList(g graph.AdjacencyList, w io.Writer, options ...func(*Config)) error {
+	cf := Defaults
+	for _, o := range options {
+		o(&cf)
+	}
+	b := bufio.NewWriter(w)
+	if err := writeHead(&cf, b); err != nil {
+		return err
+	}
+	wf := writeALUndirected
+	if cf.Directed {
+		wf = writeALDirected
+	}
+	if err := wf(g, &cf, b); err != nil {
+		return err
+	}
+	return writeTail(b)
+}
+
+func writeHead(cf *Config, b *bufio.Writer) error {
+	t := "graph"
+	if cf.Directed {
+		t = "digraph"
+	}
+	if _, err := fmt.Fprintf(b, "%s {\n", t); err != nil {
+		return err
+	}
+	for _, av := range cf.GraphAttr {
+		_, err := fmt.Fprintf(b, "%s%s = %s\n", cf.Indent, av.Attr, av.Val)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeTail(b *bufio.Writer) error {
+	if _, err := b.WriteString("}\n"); err != nil {
+		return err
+	}
+	return b.Flush()
+}
+
+func writeALDirected(g graph.AdjacencyList, cf *Config, b *bufio.Writer) error {
+	for fr, to := range g {
+		if err := writeALEdgeStmt(fr, to, "->", cf, b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeALEdgeStmt(fr int, to []graph.NI, op string, cf *Config, b *bufio.Writer) error {
+	switch len(to) {
+	case 0:
+	case 1:
+		_, err := fmt.Fprintf(b, "%s%s %s %s\n",
+			cf.Indent, cf.NodeLabel(graph.NI(fr)), op, cf.NodeLabel(to[0]))
+		if err != nil {
+			return err
+		}
+	default:
+		_, err := fmt.Fprintf(b, "%s%s %s {%s",
+			cf.Indent, cf.NodeLabel(graph.NI(fr)), op, cf.NodeLabel(to[0]))
+		if err != nil {
+			return err
+		}
+		for _, to := range to[1:] {
+			if _, err := fmt.Fprintf(b, " %s", cf.NodeLabel(to)); err != nil {
+				return err
+			}
+		}
+		if _, err := b.WriteString("}\n"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeALUndirected(g graph.AdjacencyList, cf *Config, b *bufio.Writer) error {
+	// Similar code in undir.go at IsUndirected
+	unpaired := make(graph.AdjacencyList, len(g))
+	for fr, to := range g {
+		// first collect unpaired subset of to
+		var uto []graph.NI
+	arc: // for each arc in g
+		for _, to := range to {
+			if to == graph.NI(fr) {
+				uto = append(uto, to) // loop
+				continue
+			}
+			// search unpaired arcs
+			ut := unpaired[to]
+			for i, u := range ut {
+				if u == graph.NI(fr) { // found reciprocal
+					last := len(ut) - 1
+					ut[i] = ut[last]
+					unpaired[to] = ut[:last]
+					continue arc
+				}
+			}
+			// reciprocal not found
+			uto = append(uto, to)
+			unpaired[fr] = append(unpaired[fr], to)
+		}
+		if err := writeALEdgeStmt(fr, uto, "--", cf, b); err != nil {
+			return err
+		}
+	}
+	for _, to := range unpaired {
+		if len(to) > 0 {
+			return fmt.Errorf("directed graph")
+		}
+	}
+	return nil
+}
+
+func StringLabeledAdjacencyList(g graph.LabeledAdjacencyList, options ...func(*Config)) (string, error) {
+	var b bytes.Buffer
+	if err := WriteLabeledAdjacencyList(g, &b, options...); err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
+
+func WriteLabeledAdjacencyList(g graph.LabeledAdjacencyList, w io.Writer, options ...func(*Config)) error {
+	cf := Defaults
+	for _, o := range options {
+		o(&cf)
+	}
+	b := bufio.NewWriter(w)
+	if err := writeHead(&cf, b); err != nil {
+		return err
+	}
+	wf := writeLALUndirected
+	if cf.Directed {
+		wf = writeLALDirected
+	}
+	if err := wf(g, &cf, b); err != nil {
+		return err
+	}
+	return writeTail(b)
+}
+
+func writeLALDirected(g graph.LabeledAdjacencyList, cf *Config, b *bufio.Writer) error {
+	for fr, to := range g {
+		if err := writeLALEdgeStmt(fr, to, "->", cf, b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeLALEdgeStmt(fr int, to []graph.Half, op string, cf *Config, b *bufio.Writer) error {
+	for _, to := range to {
+		_, err := fmt.Fprintf(b, "%s%s %s %s [label = %s]\n",
+			cf.Indent, cf.NodeLabel(graph.NI(fr)), op, cf.NodeLabel(to.To),
+			cf.EdgeLabel(to.Label))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeLALUndirected(g graph.LabeledAdjacencyList, cf *Config, b *bufio.Writer) error {
+	// Similar code in undir.go at IsUndirected
+	unpaired := make(graph.LabeledAdjacencyList, len(g))
+	for fr, to := range g {
+		// first collect unpaired subset of to
+		var uto []graph.Half
+	arc: // for each arc in g
+		for _, to := range to {
+			if to.To == graph.NI(fr) {
+				uto = append(uto, to) // loop
+				continue
+			}
+			// search unpaired arcs
+			ut := unpaired[to.To]
+			for i, u := range ut {
+				if u.To == graph.NI(fr) && u.Label == to.Label { // found reciprocal
+					last := len(ut) - 1
+					ut[i] = ut[last]
+					unpaired[to.To] = ut[:last]
+					continue arc
+				}
+			}
+			// reciprocal not found
+			uto = append(uto, to)
+			unpaired[fr] = append(unpaired[fr], to)
+		}
+		if err := writeLALEdgeStmt(fr, uto, "--", cf, b); err != nil {
+			return err
+		}
+	}
+	for _, to := range unpaired {
+		if len(to) > 0 {
+			return fmt.Errorf("directed graph")
+		}
+	}
+	return nil
+}
+
+func StringFromList(g graph.FromList, options ...func(*Config)) (string, error) {
+	var b bytes.Buffer
+	if err := WriteFromList(g, &b, options...); err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
+
+func WriteFromList(g graph.FromList, w io.Writer, options ...func(*Config)) error {
+	cf := Defaults
+	GraphAttr("rankdir", "BT")(&cf)
+	for _, o := range options {
+		o(&cf)
+	}
+	b := bufio.NewWriter(w)
+	if err := writeHead(&cf, b); err != nil {
+		return err
+	}
+	for n, e := range g.Paths {
+		fr := e.From
+		if fr < 0 {
+			continue
+		}
+		_, err := fmt.Fprintf(b, "%s%s -> %s\n",
+			cf.Indent, cf.NodeLabel(graph.NI(n)), cf.NodeLabel(fr))
+		if err != nil {
+			return err
+		}
+	}
+	if cf.RankLeaves {
+		if _, err := b.WriteString(cf.Indent + "{rank = same"); err != nil {
+			return err
+		}
+		for n := range g.Paths {
+			if g.Leaves.Bit(n) == 0 {
+				continue
+			}
+			_, err := b.WriteString(" " + cf.NodeLabel(graph.NI(n)))
+			if err != nil {
+				return err
+			}
+		}
+		if _, err := b.WriteString("}\n"); err != nil {
+			return err
+		}
+	}
+	return writeTail(b)
+}
+
+func StringWeightedEdgeList(g graph.WeightedEdgeList, options ...func(*Config)) (string, error) {
+	var b bytes.Buffer
+	if err := WriteWeightedEdgeList(g, &b, options...); err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
+
+func WriteWeightedEdgeList(g graph.WeightedEdgeList, w io.Writer, options ...func(*Config)) error {
+	cf := Defaults
+	cf.Directed = false
+	cf.EdgeLabel = func(l int) string {
+		return fmt.Sprintf(`"%g"`, g.WeightFunc(l))
+	}
+	for _, o := range options {
+		o(&cf)
+	}
+	if cf.UndirectArcs {
+		cf.Directed = false
+	}
+	b := bufio.NewWriter(w)
+	if err := writeHead(&cf, b); err != nil {
+		return err
+	}
+	wf := writeWELNoRecip
+	if cf.UndirectArcs || cf.Directed {
+		wf = writeWELAllArcs
+	}
+	if err := wf(g, &cf, b); err != nil {
+		return err
+	}
+	return writeTail(b)
+}
+
+func writeWELNoRecip(g graph.WeightedEdgeList, cf *Config, b *bufio.Writer) error {
+	unpaired := make(graph.LabeledAdjacencyList, g.Order)
+edge:
+	for _, e := range g.Edges {
+		// search unpaired arcs
+		u2 := unpaired[e.N2]
+		for i, u := range u2 {
+			if u.To == e.N1 && u.Label == e.Label { // found reciprocal
+				// write the edge
+				_, err := fmt.Fprintf(b, "%s%s -- %s [label = %s]\n",
+					cf.Indent, cf.NodeLabel(e.N2), cf.NodeLabel(e.N1),
+					cf.EdgeLabel(e.Label))
+				if err != nil {
+					return err
+				}
+				// delete reciprocal
+				last := len(u2) - 1
+				u2[i] = u2[last]
+				unpaired[e.N2] = u2[:last]
+				continue edge
+			}
+		}
+		// reciprocal not found
+		unpaired[e.N1] = append(unpaired[e.N1], graph.Half{e.N2, e.Label})
+	}
+	for _, to := range unpaired {
+		if len(to) > 0 {
+			return fmt.Errorf("directed graph")
+		}
+	}
+	return nil
+}
+
+func writeWELAllArcs(g graph.WeightedEdgeList, cf *Config, b *bufio.Writer) error {
+	op := "--"
+	if cf.Directed {
+		op = "->"
+	}
+	for _, e := range g.Edges {
+		_, err := fmt.Fprintf(b, "%s%s %s %s [label = %s]\n",
+			cf.Indent, cf.NodeLabel(e.N1), op, cf.NodeLabel(e.N2),
+			cf.EdgeLabel(e.Label))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ dot/dot_test.go
@@ -0,0 +1,339 @@
+package dot_test
+
+import (
+	"fmt"
+
+	"github.com/soniakeys/graph"
+	"github.com/soniakeys/graph/dot"
+)
+
+func ExampleStringAdjacencyList() {
+	// arcs directed down:
+	// 0  4
+	// | /|
+	// |/ |
+	// 2  3
+	g := graph.AdjacencyList{
+		0: {2},
+		4: {2, 3},
+	}
+	// (default indent is 2)
+	s, _ := dot.StringAdjacencyList(g, dot.Indent(""))
+	fmt.Println(s)
+	// Output:
+	// digraph {
+	// 0 -> 2
+	// 4 -> {2 3}
+	// }
+}
+
+func ExampleIndent() {
+	// arcs directed down:
+	// 0  4
+	// | /|
+	// |/ |
+	// 2  3
+	g := graph.AdjacencyList{
+		0: {2},
+		4: {2, 3},
+	}
+	// All other examples have Indent("") to avoid a quirk of go test
+	// that it can't handle leading space in the output.  In this example a
+	// nonbreaking space works around the quirk to show indented output that
+	// looks like the default two space indent.
+	// (But then if you render it with graphviz, graphviz picks up the nbsp
+	// as a node statement...)
+	s, _ := dot.StringAdjacencyList(g, dot.Indent("\u00a0 "))
+	fmt.Println(s)
+	// Output:
+	// digraph {
+	//   0 -> 2
+	//   4 -> {2 3}
+	// }
+}
+
+func ExampleDirected() {
+	// arcs directed down:
+	// 0  2
+	// | /|
+	// |/ |
+	// 3  4
+	g := graph.AdjacencyList{
+		0: {3},
+		2: {3, 4},
+		4: {},
+	}
+	_, err := dot.StringAdjacencyList(g, dot.Directed(false))
+	fmt.Println("Error:", err)
+	fmt.Println()
+
+	g = g.UndirectedCopy()
+	s, _ := dot.StringAdjacencyList(g, dot.Directed(false), dot.Indent(""))
+	fmt.Println(s)
+	// Output:
+	// Error: directed graph
+	//
+	// graph {
+	// 0 -- 3
+	// 2 -- {3 4}
+	// }
+}
+
+func ExampleNodeLabel() {
+	// arcs directed down:
+	// A  D
+	// | /|
+	// |/ |
+	// B  C
+	labels := []string{
+		0: "A",
+		4: "D",
+		2: "B",
+		3: "C",
+	}
+	lf := func(n graph.NI) string { return labels[n] }
+	g := graph.AdjacencyList{
+		0: {2},
+		4: {2, 3},
+	}
+	s, _ := dot.StringAdjacencyList(g, dot.Indent(""), dot.NodeLabel(lf))
+	fmt.Println(s)
+	// Output:
+	// digraph {
+	// A -> B
+	// D -> {B C}
+	// }
+}
+
+func ExampleNodeLabel_construction() {
+	// arcs directed down:
+	// A  D
+	// | /|
+	// |/ |
+	// B  C
+	var g graph.AdjacencyList
+
+	// graph construction mechanism
+	labels := []string{}
+	nodes := map[string]graph.NI{}
+	node := func(l string) graph.NI {
+		if n, ok := nodes[l]; ok {
+			return n
+		}
+		n := graph.NI(len(labels))
+		labels = append(labels, l)
+		g = append(g, nil)
+		nodes[l] = n
+		return n
+	}
+	addArc := func(fr, to string) {
+		f := node(fr)
+		g[f] = append(g[f], node(to))
+	}
+
+	// construct graph
+	addArc("A", "B")
+	addArc("D", "B")
+	addArc("D", "C")
+
+	// generate dot
+	lf := func(n graph.NI) string { return labels[n] }
+	s, _ := dot.StringAdjacencyList(g, dot.Indent(""), dot.NodeLabel(lf))
+	fmt.Println(s)
+
+	// Output:
+	// digraph {
+	// A -> B
+	// D -> {B C}
+	// }
+}
+
+func ExampleStringLabeledAdjacencyList() {
+	// arcs directed down:
+	//     0      4
+	// (30)|     /|
+	//     | (20) |
+	//     |/     |(10)
+	//     2      3
+	g := graph.LabeledAdjacencyList{
+		0: {{2, 30}},
+		4: {{2, 20}, {3, 10}},
+	}
+	s, _ := dot.StringLabeledAdjacencyList(g, dot.Indent(""))
+	fmt.Println(s)
+	// Output:
+	// digraph {
+	// 0 -> 2 [label = 30]
+	// 4 -> 2 [label = 20]
+	// 4 -> 3 [label = 10]
+	// }
+}
+
+func ExampleEdgeLabel() {
+	// arcs directed down:
+	//      0       4
+	// (.33)|      /|
+	//      | (1.7) |
+	//      |/      |(2e117)
+	//      2       3
+	weights := map[int]float64{
+		30: .33,
+		20: 1.7,
+		10: 2e117,
+	}
+	lf := func(l int) string { return fmt.Sprintf(`"%g"`, weights[l]) }
+	g := graph.LabeledAdjacencyList{
+		0: {{2, 30}},
+		4: {{2, 20}, {3, 10}},
+	}
+	s, _ := dot.StringLabeledAdjacencyList(g, dot.EdgeLabel(lf), dot.Indent(""))
+	fmt.Println(s)
+	// Output:
+	// digraph {
+	// 0 -> 2 [label = "0.33"]
+	// 4 -> 2 [label = "1.7"]
+	// 4 -> 3 [label = "2e+117"]
+	// }
+}
+
+func ExampleStringFromList() {
+	//   0
+	//  / \
+	// 1   2
+	//      \
+	//       3
+	f := graph.FromList{Paths: []graph.PathEnd{
+		0: {From: -1},
+		1: {From: 0},
+		2: {From: 0},
+		3: {From: 2},
+	}}
+	s, _ := dot.StringFromList(f, dot.Indent(""))
+	fmt.Println(s)
+	// Output:
+	// digraph {
+	// rankdir = BT
+	// 1 -> 0
+	// 2 -> 0
+	// 3 -> 2
+	// }
+}
+
+func ExampleStringFromList_leavesRanked() {
+	//   0
+	//  / \
+	// 1   2
+	//      \
+	//       3
+	f := graph.FromList{Paths: []graph.PathEnd{
+		0: {From: -1},
+		1: {From: 0},
+		2: {From: 0},
+		3: {From: 2},
+	}}
+	f.Leaves.SetBit(&f.Leaves, 1, 1)
+	f.Leaves.SetBit(&f.Leaves, 3, 1)
+	s, _ := dot.StringFromList(f, dot.Indent(""), dot.RankLeaves(true))
+	fmt.Println(s)
+	// Output:
+	// digraph {
+	// rankdir = BT
+	// 1 -> 0
+	// 2 -> 0
+	// 3 -> 2
+	// {rank = same 1 3}
+	// }
+}
+
+func ExampleGraphAttr() {
+	// arcs directed down:
+	// 0  4
+	// | /|
+	// |/ |
+	// 2  3
+	g := graph.AdjacencyList{
+		0: {2},
+		4: {2, 3},
+	}
+	s, _ := dot.StringAdjacencyList(g,
+		dot.GraphAttr("bgcolor", "honeydew"),
+		dot.Indent(""))
+	fmt.Println(s)
+	// Output:
+	// digraph {
+	// bgcolor = honeydew
+	// 0 -> 2
+	// 4 -> {2 3}
+	// }
+}
+
+func ExampleStringWeigtedEdgeList() {
+	//              (label 0, wt 1.6)
+	//          0----------------------2
+	// (label 1 |                     /
+	//  wt .33) |  ------------------/
+	//          | / (label 2, wt 1.7)
+	//          |/
+	//          1
+	weights := []float64{
+		0: 1.6,
+		1: .33,
+		2: 1.7,
+	}
+	g := graph.WeightedEdgeList{
+		WeightFunc: func(l int) float64 { return weights[l] },
+		Order:      3,
+		Edges: []graph.LabeledEdge{
+			{graph.Edge{0, 1}, 1},
+			{graph.Edge{1, 0}, 1},
+
+			{graph.Edge{0, 2}, 0},
+			{graph.Edge{2, 0}, 0},
+
+			{graph.Edge{1, 2}, 2},
+			{graph.Edge{2, 1}, 2},
+		},
+	}
+	s, _ := dot.StringWeightedEdgeList(g, dot.Indent(""))
+	fmt.Println(s)
+	// Output:
+	// graph {
+	// 0 -- 1 [label = "0.33"]
+	// 0 -- 2 [label = "1.6"]
+	// 1 -- 2 [label = "1.7"]
+	// }
+}
+
+func ExampleUndirectArcs() {
+	//              (label 0, wt 1.6)
+	//          0----------------------2
+	// (label 1 |                     /
+	//  wt .33) |  ------------------/
+	//          | / (label 2, wt 1.7)
+	//          |/
+	//          1
+	weights := []float64{
+		0: 1.6,
+		1: .33,
+		2: 1.7,
+	}
+	g := graph.WeightedEdgeList{
+		WeightFunc: func(l int) float64 { return weights[l] },
+		Order:      3,
+		Edges: []graph.LabeledEdge{
+			{graph.Edge{0, 1}, 1},
+			{graph.Edge{0, 2}, 0},
+			{graph.Edge{1, 2}, 2},
+		},
+	}
+	s, _ := dot.StringWeightedEdgeList(g,
+		dot.UndirectArcs(true),
+		dot.Indent(""))
+	fmt.Println(s)
+	// Output:
+	// graph {
+	// 0 -- 1 [label = "0.33"]
+	// 0 -- 2 [label = "1.6"]
+	// 1 -- 2 [label = "1.7"]
+	// }
+}
