989aaeaa5645f39ab50982123fd8836f6e64d55a
Sonia Keys <soniakeys@gmail.com>
2016-03-07 22:20:18 -0500

    split files

--- dot/dot_test.go
+++ dot/dot_test.go
@@ -2,7 +2,6 @@ package dot_test
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/soniakeys/graph"
 	"github.com/soniakeys/graph/dot"
@@ -48,163 +47,6 @@ func ExampleStringAdjacencyList_parallelArcs() {
 	// }
 }
 
-func ExampleGraphAttr() {
-	// arcs directed right:
-	// 0---2
-	//  \ / \
-	//   1---3
-	g := graph.AdjacencyList{
-		0: {1, 2},
-		1: {2, 3},
-		2: {3},
-		3: {},
-	}
-	dot.WriteAdjacencyList(g, os.Stdout,
-		dot.GraphAttr("rankdir", "LR"), dot.Indent(""))
-	// Output:
-	// digraph {
-	// rankdir = LR
-	// 0 -> {1 2}
-	// 1 -> {2 3}
-	// 2 -> 3
-	// }
-}
-
-func ExampleIndent() {
-	// arcs directed down:
-	// 0  4
-	// | /|
-	// |/ |
-	// 2  3
-	g := graph.AdjacencyList{
-		0: {2},
-		4: {2, 3},
-	}
-	// All other examples have Indent("") to avoid a quirk of go test
-	// that it can't handle leading space in the output.  In this example a
-	// nonbreaking space works around the quirk to show indented output that
-	// looks like the default two space indent.
-	// (But then if you render it with graphviz, graphviz picks up the nbsp
-	// as a node statement...)
-	s, _ := dot.StringAdjacencyList(g, dot.Indent("\u00a0 "))
-	fmt.Println(s)
-	// Output:
-	// digraph {
-	//   0 -> 2
-	//   4 -> {2 3}
-	// }
-}
-
-func ExampleDirected() {
-	// arcs directed down:
-	// 0  2
-	// | /|
-	// |/ |
-	// 3  4
-	g := graph.Directed{graph.AdjacencyList{
-		0: {3},
-		2: {3, 4},
-		4: {},
-	}}
-	// default is directed
-	s, _ := dot.StringAdjacencyList(g.AdjacencyList, dot.Indent(""))
-	fmt.Println(s)
-	fmt.Println()
-
-	// Directed(false) generates error witout reciprocal arcs
-	_, err := dot.StringAdjacencyList(g.AdjacencyList, dot.Directed(false))
-	fmt.Println("Error:", err)
-	fmt.Println()
-
-	// undirected
-	u := g.Undirected()
-	s, _ = dot.StringAdjacencyList(u.AdjacencyList,
-		dot.Directed(false), dot.Indent(""))
-	fmt.Println(s)
-
-	// Output:
-	// digraph {
-	// 0 -> 3
-	// 2 -> {3 4}
-	// }
-	//
-	// Error: directed graph
-	//
-	// graph {
-	// 0 -- 3
-	// 2 -- {3 4}
-	// }
-}
-
-func ExampleNodeLabel() {
-	// arcs directed down:
-	// A  D
-	// | /|
-	// |/ |
-	// B  C
-	labels := []string{
-		0: "A",
-		4: "D",
-		2: "B",
-		3: "C",
-	}
-	lf := func(n graph.NI) string { return labels[n] }
-	g := graph.AdjacencyList{
-		0: {2},
-		4: {2, 3},
-	}
-	s, _ := dot.StringAdjacencyList(g, dot.Indent(""), dot.NodeLabel(lf))
-	fmt.Println(s)
-	// Output:
-	// digraph {
-	// A -> B
-	// D -> {B C}
-	// }
-}
-
-func ExampleNodeLabel_construction() {
-	// arcs directed down:
-	// A  D
-	// | /|
-	// |/ |
-	// B  C
-	var g graph.AdjacencyList
-
-	// example graph construction mechanism
-	labels := []string{}
-	nodes := map[string]graph.NI{}
-	node := func(l string) graph.NI {
-		if n, ok := nodes[l]; ok {
-			return n
-		}
-		n := graph.NI(len(labels))
-		labels = append(labels, l)
-		g = append(g, nil)
-		nodes[l] = n
-		return n
-	}
-	addArc := func(fr, to string) {
-		f := node(fr)
-		g[f] = append(g[f], node(to))
-	}
-
-	// construct graph
-	addArc("A", "B")
-	addArc("D", "B")
-	addArc("D", "C")
-
-	// generate dot
-	lf := func(n graph.NI) string { return labels[n] }
-	s, _ := dot.StringAdjacencyList(g, dot.Indent(""), dot.NodeLabel(lf))
-	fmt.Println(s)
-
-	// Output:
-	// digraph {
-	// A -> B
-	// D -> {B C}
-	// }
-}
-
 func ExampleStringLabeledAdjacencyList() {
 	// arcs directed down:
 	//     0      4
@@ -223,35 +65,6 @@ func ExampleStringLabeledAdjacencyList() {
 	// 0 -> 2 [label = 30]
 	// 4 -> 2 [label = 20]
 	// 4 -> 3 [label = 10]
-	// }
-}
-
-func ExampleEdgeLabel() {
-	// arcs directed down:
-	//      0       4
-	// (.33)|      /|
-	//      | (1.7) |
-	//      |/      |(2e117)
-	//      2       3
-	weights := map[int]float64{
-		30: .33,
-		20: 1.7,
-		10: 2e117,
-	}
-	lf := func(l graph.LI) string {
-		return fmt.Sprintf(`"%g"`, weights[int(l)])
-	}
-	g := graph.LabeledAdjacencyList{
-		0: {{2, 30}},
-		4: {{2, 20}, {3, 10}},
-	}
-	s, _ := dot.StringLabeledAdjacencyList(g, dot.EdgeLabel(lf), dot.Indent(""))
-	fmt.Println(s)
-	// Output:
-	// digraph {
-	// 0 -> 2 [label = "0.33"]
-	// 4 -> 2 [label = "1.7"]
-	// 4 -> 3 [label = "2e+117"]
 	// }
 }
 
@@ -309,40 +122,6 @@ func ExampleStringWeightedEdgeList() {
 		},
 	}
 	s, _ := dot.StringWeightedEdgeList(g, dot.Indent(""))
-	fmt.Println(s)
-	// Output:
-	// graph {
-	// 0 -- 1 [label = "0.33"]
-	// 0 -- 2 [label = "1.6"]
-	// 1 -- 2 [label = "1.7"]
-	// }
-}
-
-func ExampleUndirectArcs() {
-	//              (label 0, wt 1.6)
-	//          0----------------------2
-	// (label 1 |                     /
-	//  wt .33) |  ------------------/
-	//          | / (label 2, wt 1.7)
-	//          |/
-	//          1
-	weights := []float64{
-		0: 1.6,
-		1: .33,
-		2: 1.7,
-	}
-	g := graph.WeightedEdgeList{
-		WeightFunc: func(l graph.LI) float64 { return weights[int(l)] },
-		Order:      3,
-		Edges: []graph.LabeledEdge{
-			{graph.Edge{0, 1}, 1},
-			{graph.Edge{0, 2}, 0},
-			{graph.Edge{1, 2}, 2},
-		},
-	}
-	s, _ := dot.StringWeightedEdgeList(g,
-		dot.UndirectArcs(true),
-		dot.Indent(""))
 	fmt.Println(s)
 	// Output:
 	// graph {
---
+++ dot/options.go
@@ -0,0 +1,106 @@
+package dot
+
+import (
+	"strconv"
+
+	"github.com/soniakeys/graph"
+)
+
+// AttrVal represents the dot format concept of an attribute-value pair.
+type AttrVal struct {
+	Attr string
+	Val  string
+}
+
+// Config holds options that control the dot output.
+//
+// See Overview/Scheme for an overview of how this works.  Generally you will
+// not set members of a Config struct directly.  There is an option function
+// for each member.  To set a member, pass the option function as an optional
+// argument to a Write or String function.
+type Config struct {
+	Directed     bool
+	EdgeLabel    func(graph.LI) string
+	GraphAttr    []AttrVal
+	Indent       string
+	NodeLabel    func(graph.NI) string
+	UndirectArcs bool
+}
+
+// Defaults holds a package default Config struct.
+//
+// Defaults is copied as the first configuration step.  See Overview/Scheme.
+var Defaults = Config{
+	Directed:  true,
+	EdgeLabel: func(l graph.LI) string { return strconv.Itoa(int(l)) },
+	Indent:    "  ",
+	NodeLabel: func(n graph.NI) string { return strconv.Itoa(int(n)) },
+}
+
+// Directed specifies whether to write a dot format directected or undirected
+// graph.
+//
+// The default, Directed(true), specifies a dot format directected graph,
+// or "digraph."  In this case the Write or String function outputs each arc
+// of the graph as a dot format directed edge.
+//
+// Directed(false) specifies a dot format undirected graph or simply a "graph."
+// In this case the Write or String function requires that all arcs between
+// distinct nodes occur in reciprocal pairs.  For each pair the function
+// outputs a single edge in dot format.
+func Directed(d bool) func(*Config) {
+	return func(c *Config) { c.Directed = d }
+}
+
+// EdgeLabel specifies a function to generate edge label strings for the
+// dot format given the arc label integers of graph package.
+//
+// The default function is simply strconv.Itoa of the graph package arc label.
+func EdgeLabel(f func(graph.LI) string) func(*Config) {
+	return func(c *Config) { c.EdgeLabel = f }
+}
+
+// GraphAttr adds a dot format graph attribute.
+//
+// Graph attributes are held in a slice, and so are ordered.  This function
+// updates the value of the last matching attribute if it exists, or adds a
+// new attribute to the end of the list.
+func GraphAttr(attr, val string) func(*Config) {
+	return func(c *Config) {
+		for i := len(c.GraphAttr) - 1; i >= 0; i-- {
+			if c.GraphAttr[i].Attr == attr {
+				c.GraphAttr[i].Val = val
+				return
+			}
+		}
+		c.GraphAttr = append(c.GraphAttr, AttrVal{attr, val})
+	}
+}
+
+// Indent specifies an indent string for the body of the dot format.
+//
+// The default is two spaces.
+func Indent(i string) func(*Config) {
+	return func(c *Config) { c.Indent = i }
+}
+
+// NodeLabel specifies a function to generate node label strings for the
+// dot format given the node integers of graph package.
+//
+// The default function is simply strconv.Itoa of the graph package node
+// integer.
+func NodeLabel(f func(graph.NI) string) func(*Config) {
+	return func(c *Config) { c.NodeLabel = f }
+}
+
+// UndirectArcs, for the WeightedEdgeList graph type, specifies to write
+// each element of the edge list as a dot file undirected edge.
+//
+// Note that while Directed(false) requires arcs to occur in reciprocal pairs,
+// UndirectArcs(true) does not, and does not collapse reciprocal arc pairs to
+// single dot format edges.
+//
+// See WriteWeightedEdgeList for more detail.
+func UndirectArcs(u bool) func(*Config) {
+	return func(c *Config) { c.UndirectArcs = u }
+}
