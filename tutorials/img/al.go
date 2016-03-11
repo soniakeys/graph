30383d12ca8b1ee7706b28c607f3db5046ee4a30
Sonia Keys <soniakeys@gmail.com>
2016-03-10 23:09:34 -0500

    go generate generates svgs

--- tutorials/img/al.go
+++ tutorials/img/al.go
@@ -1,3 +1,4 @@
+// go generate in this directory generates images for the tutorials.
 package main
 
 import (
@@ -8,8 +9,11 @@ import (
 	"github.com/soniakeys/graph/dot"
 )
 
+//go:generate go run al.go
+//go:generate dia -t svg -e almem.svg almem.dia
+
 func main() {
-	for _, f := range []func() error{al} {
+	for _, f := range []func() error{al, al0, alpair, ald} {
 		if err := f(); err != nil {
 			log.Fatal(err)
 		}
@@ -43,13 +47,51 @@ func al0() error {
 		3: {5},
 		6: {5, 6},
 	}
+	// al0 output file name
 	c := exec.Command("dot", "-Tsvg", "-o", "al0.svg")
 	w, err := c.StdinPipe()
 	if err != nil {
 		return err
 	}
 	c.Start()
+	// otherwise, only difference from al() is dot.Isolated
+	dot.WriteAdjacencyList(g, w,
+		dot.GraphAttr("rankdir", "LR"), dot.Isolated(true))
+	w.Close()
+	return c.Wait()
+}
+
+func alpair() error {
+	g := graph.AdjacencyList{
+		0: {1},
+		1: {0},
+	}
+	c := exec.Command("dot", "-Tsvg", "-o", "alpair.svg")
+	w, err := c.StdinPipe()
+	if err != nil {
+		return err
+	}
+	c.Start()
 	dot.WriteAdjacencyList(g, w, dot.GraphAttr("rankdir", "LR"))
+	w.Close()
+	return c.Wait()
+}
+
+func ald() error {
+	g := graph.LabeledAdjacencyList{
+		1: {{To: 2, Label: 7}, {To: 3, Label: 9}, {To: 6, Label: 11}},
+		2: {{To: 3, Label: 10}, {To: 4, Label: 15}},
+		3: {{To: 4, Label: 11}, {To: 6, Label: 2}},
+		4: {{To: 5, Label: 7}},
+		6: {{To: 5, Label: 9}},
+	}
+	c := exec.Command("dot", "-Tsvg", "-o", "ald.svg")
+	w, err := c.StdinPipe()
+	if err != nil {
+		return err
+	}
+	c.Start()
+	dot.WriteLabeledAdjacencyList(g, w, dot.GraphAttr("rankdir", "LR"))
 	w.Close()
 	return c.Wait()
 }
