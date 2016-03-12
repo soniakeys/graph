402d79a6968a932dc96f5a596427fbb1c46cd831
Sonia Keys <soniakeys@gmail.com>
2016-03-12 10:44:22 -0500

    treevis

--- treevis/treevis.go
+++ treevis/treevis.go
@@ -1,43 +1,89 @@
 // Copyright 2016 Sonia Keys
 // License MIT: http://opensource.org/licenses/MIT
 
+// Treevis draws trees with text.  Capabilities are currently quite limited.
 package treevis
 
 import (
 	"fmt"
 	"io"
 	"math/big"
+	"strconv"
 
 	"github.com/soniakeys/graph"
 )
 
-func WriteDirected(g graph.Directed, root graph.NI, label func(graph.NI) string, w io.Writer) {
-	if len(g.AdjacencyList) == 0 {
-		fmt.Fprintln(w, "<empty>")
-		return
+type G struct {
+	Leaf      string
+	NonLeaf   string
+	Child     string
+	Vertical  string
+	LastChild string
+	Indent    string
+}
+
+type Config struct {
+	NodeLabel func(graph.NI) string
+	Glyphs    G
+}
+
+var Defaults = Config{
+	NodeLabel: func(n graph.NI) string { return strconv.Itoa(int(n)) },
+	Glyphs: G{
+		Leaf:      "╴",
+		NonLeaf:   "┐",
+		Child:     "├─",
+		Vertical:  "│ ",
+		LastChild: "└─",
+		Indent:    "  ",
+	},
+}
+
+func NodeLabel(f func(graph.NI) string) func(*Config) {
+	return func(c *Config) { c.NodeLabel = f }
+}
+
+func Glyphs(g G) func(*Config) {
+	return func(c *Config) { c.Glyphs = g }
+}
+
+func Write(g graph.Directed, root graph.NI, w io.Writer, options ...func(*Config)) (err error) {
+	cf := Defaults
+	for _, o := range options {
+		o(&cf)
 	}
 	var vis big.Int
 	var f func(graph.NI, string) bool
 	f = func(n graph.NI, pre string) bool {
 		if vis.Bit(int(n)) != 0 {
-			fmt.Fprintln(w, "<cycle detected>")
+			fmt.Fprintln(w, "%!(NONTREE)")
+			err = fmt.Errorf("non-tree")
 			return false
 		}
 		vis.SetBit(&vis, int(n), 1)
 		to := g.AdjacencyList[n]
 		if len(to) == 0 {
-			fmt.Fprintln(w, "╴", label(n))
-			return true
+			_, err = fmt.Fprint(w, cf.Glyphs.Leaf, cf.NodeLabel(n), "\n")
+			return err == nil
 		}
-		fmt.Fprintln(w, "┐", label(n))
+		_, err = fmt.Fprint(w, cf.Glyphs.NonLeaf, cf.NodeLabel(n), "\n")
+		if err != nil {
+			return false
+		}
 		last := len(to) - 1
 		for _, to := range to[:last] {
-			fmt.Fprint(w, pre, "├─")
-			f(to, pre+"│ ")
+			if _, err = fmt.Fprint(w, pre, cf.Glyphs.Child); err != nil {
+				return false
+			}
+			if !f(to, pre+cf.Glyphs.Vertical) {
+				return false
+			}
 		}
-		fmt.Fprint(w, pre, "└─")
-		f(to[last], pre+"  ")
-		return true
+		if _, err = fmt.Fprint(w, pre, cf.Glyphs.LastChild); err != nil {
+			return false
+		}
+		return f(to[last], pre+cf.Glyphs.Indent)
 	}
 	f(root, "")
+	return
 }
--- treevis/treevis_test.go
+++ treevis/treevis_test.go
@@ -10,31 +10,75 @@ import (
 	"github.com/soniakeys/graph/treevis"
 )
 
-func ExampleWriteDirected() {
+func ExampleWrite() {
 	g := graph.Directed{graph.AdjacencyList{
-		0: {1, 2, 3}, // root
-		1: {4, 5},    // ei
-		5: {6},       // y
-		6: {},        // eff
+		0: {1, 2, 3},
+		1: {4, 5},
+		5: {6},
+		6: {},
+	}}
+	treevis.Write(g, 0, os.Stdout)
+	// Output:
+	// ┐0
+	// ├─┐1
+	// │ ├─╴4
+	// │ └─┐5
+	// │   └─╴6
+	// ├─╴2
+	// └─╴3
+}
+
+func ExampleGlyphs() {
+	g := graph.Directed{graph.AdjacencyList{
+		0: {1, 2, 3},
+		1: {4, 5},
+		5: {6},
+		6: {},
+	}}
+	treevis.Write(g, 0, os.Stdout, treevis.Glyphs(treevis.G{
+		Leaf:      "-",
+		NonLeaf:   "-",
+		Child:     " |",
+		Vertical:  " |",
+		LastChild: " `",
+		Indent:    "  ",
+	}))
+	// Output:
+	// -0
+	//  |-1
+	//  | |-4
+	//  | `-5
+	//  |   `-6
+	//  |-2
+	//  `-3
+}
+
+func ExampleNodeLabel() {
+	g := graph.Directed{graph.AdjacencyList{
+		0: {1, 2, 3},
+		1: {4, 5},
+		5: {6},
+		6: {},
 	}}
 	labels := []string{
 		0: "root",
-		1: "ei",
-		2: "bee",
-		3: "si",
-		4: "dee",
-		5: "y",
-		6: "eff",
+		1: "a",
+		2: "b",
+		3: "c",
+		4: "d",
+		5: "e",
+		6: "f",
 	}
-	treevis.WriteDirected(g, 0,
-		func(n graph.NI) string { return labels[n] },
-		os.Stdout)
+	treevis.Write(g, 0, os.Stdout,
+		treevis.NodeLabel(func(n graph.NI) string {
+			return " " + labels[n]
+		}))
 	// Output:
 	// ┐ root
-	// ├─┐ ei
-	// │ ├─╴ dee
-	// │ └─┐ y
-	// │   └─╴ eff
-	// ├─╴ bee
-	// └─╴ si
+	// ├─┐ a
+	// │ ├─╴ d
+	// │ └─┐ e
+	// │   └─╴ f
+	// ├─╴ b
+	// └─╴ c
 }
