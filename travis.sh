5e690f4ada6989cf6121ebf9a8299054ea7d20d7
Sonia Keys <skeys@cfa.harvard.edu>
2016-04-04 16:49:48 +0000

    extra tests for 1.6 only

--- travis.sh
+++ travis.sh
@@ -4,7 +4,6 @@ go test ./...
 if [ "$TRAVIS_GO_VERSION" = "1.6" ]; then
  GOARCH=386 go test ./...
  go tool vet -example .
- false
  go get github.com/client9/misspell/cmd/misspell
  go get github.com/soniakeys/vetc
  misspell * **/*
