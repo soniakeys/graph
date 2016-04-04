3e001f46f5dde76e137d192ae9f4d4799398b7c2
Sonia Keys <skeys@cfa.harvard.edu>
2016-04-04 16:18:41 +0000

    debug try

--- travis.sh
+++ travis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 go test ./...
-echo ${TRAVIS_GO_VERSION}
-if [ "${TRAVIS_GO_VERSION}" = "go1.6" ]; then
+echo $TRAVIS_GO_VERSION
+if [ "$TRAVIS_GO_VERSION" = "1.6" ]; then
  GOARCH=386 go test ./...
  go tool vet -example .
  go get github.com/client9/misspell/cmd/misspell
